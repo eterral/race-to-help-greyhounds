c6df1cda0c1f6c621fd0dc296533bdd3d7089fe6
eterral <elliott@eterral.com>
2021-12-05 15:47:51 -0600

    added second useeffect for racetracks table

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,37 @@ import update from "react-addons-update";
 import { Routes, Route, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { grabRescues } from "./services";
+import { grabRacetracks } from "./services";
 import Header from "./components/Header";
 import About from "./components/About";
 import Rescues from "./components/Rescues";
 
 function App() {
   const [rescues, setRescues] = useState([]);
-  const [states, setStates] = useState([]);
+  const [racetracks, setRacetracks] = useState([]);
+  const [rescueStates, setRescueStates] = useState([]);
+  const [racetrackStates, setRacetrackStates] = useState([]);
 
   useEffect(() => {
     const getAllRescues = async () => {
       const response = await grabRescues();
       setRescues(response);
-      setStates([...new Set(response.map((rescue) => rescue.fields.state))]);
+      setRescueStates([
+        ...new Set(response.map((rescue) => rescue.fields.state)),
+      ]);
     };
     getAllRescues();
+  }, []);
+
+  useEffect(() => {
+    const getAllRacetracks = async () => {
+      const response = await grabRacetracks();
+      setRacetracks(response);
+      setRacetrackStates([
+        ...new Set(response.map((racetracks) => racetracks.fields.state)),
+      ]);
+    };
+    getAllRacetracks();
   }, []);
 
   return (
@@ -27,7 +43,7 @@ function App() {
         <Route path="/" element={<About />} />
         <Route
           path="/rescues"
-          element={<Rescues states={states} rescues={rescues} />}
+          element={<Rescues states={rescueStates} rescues={rescues} />}
         />
         <Route path="/racetracks" element={<Racetracks />} />
       </Routes>
