9d27616fbe2a783efdad428521130f45bd9d9c62
eterral <elliott@eterral.com>
2021-12-05 15:44:38 -0600

    added racetracks table get request

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const BASE_URL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/rescues`;
+export const BASE_URL_RESCUES = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/rescues`;
+export const BASE_URL_RACETRACKS = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/racetracks`;
 export const config = {
   headers: {
     Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
@@ -8,6 +9,11 @@ export const config = {
 };
 
 export const grabRescues = async () => {
-  const response = await axios.get(BASE_URL, config);
+  const response = await axios.get(BASE_URL_RESCUES, config);
+  return response.data.records;
+};
+
+export const grabRacetracks = async () => {
+  const response = await axios.get(BASE_URL_RACETRACKS, config);
   return response.data.records;
 };
