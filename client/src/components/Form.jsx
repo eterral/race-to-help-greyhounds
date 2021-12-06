9c1b13d582d95ee748e6258d16ba8f9cc7c7c350
eterral <elliott@eterral.com>
2021-12-06 09:22:14 -0600

    added form

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function Form() {
+    const [name, setName] = useState('')
+    const [name, setWebsite] = useState('')
+    const [name, setState] = useState('')
+    const [name, setContact] = useState('')
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const newRescue = {
+            name,
+            website,
+            state,
+            contact
+        }
+        setToggle(prevToggle => !prevToggle)
+        await postRescue(newRescue)
+        navigate('/')
+    }
+
     return (
-        <div>
-            
-        </div>
+        <form onSubmit={handleSubmit}>
+            <input 
+                type="text" 
+                value={name} 
+                name='name' 
+                onChange={(e) => setName(e.target.value)}/>
+            <input 
+                type="text" 
+                value={website} 
+                name='website' 
+                onChange={(e) => setWebsite(e.target.value)}/>
+            <input 
+                type="text" 
+                value={state} 
+                name='state' 
+                onChange={(e) => setState(e.target.value)}/>
+            <input 
+                type="text" 
+                value={contact} 
+                name='contact' 
+                onChange={(e) => setContact(e.target.value)}/> 
+            <button>Add Rescue</button>
+        </form>
     )
 }
