3fe3821d42e0519f32be206ce5b4fa4e32285b8e
eterral <elliott@eterral.com>
2021-12-08 12:06:58 -0600

    working on header image

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Header.css'
+import {Link} from 'react-router-dom'
 
 export default function Header() {
     return (
         <div className="header">
-            <p>Race To Help Greyhounds</p> 
+            <Link to="/"><p className="headerText">Race To Help Greyhounds</p></Link>
         </div>
     )
 }
